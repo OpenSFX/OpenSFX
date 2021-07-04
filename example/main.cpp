e40ebd6016e76b475abd2c5d6d7524e48568f47c
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2021-07-04 18:42:19 -0500

    Example and more function declarations

--- example/main.cpp
+++ example/main.cpp
@@ -5,7 +5,33 @@ int main(int argc, const char** argv)
 {
     std::cout << "Audio Lib Example" << std::endl;
 
+    if (argc < 2)
+    {
+        std::cout << "No audio file specified" << std::endl;
+        return 0;
+    }
 
+    const char* path = argv[1];
+
+    std::cout << "Loading File: '" << path << "'..." << std::endl;
+    AudioFile* file = sfx_load_audio(path);
+
+    std::cout << "Creating Source..." << std::endl;
+    AudioSource* source = sfx_create_source(file);
+
+    std::cout << "Freeing File..." << std::endl;
+    sfx_free_audio(file);
+
+    std::cout << "Playing Source..." << std::endl;
+    sfx_play_source(source);
+
+    std::cout << "Waiting for source to finish playing..." << std::endl;
+    while (sfx_get_status(source));
+
+    std::cout << "Freeing Source..." << std::endl;
+    sfx_free_source(source);
+
+    std::cout << "Done." << std::endl;
 
     return 0;
 }
--- include/AudioLib.h
+++ include/AudioLib.h
@@ -15,35 +15,39 @@ extern "C"
         STOPPED
     };
 
-    AudioFile* load_audio(const char* file);
-    void free_audio(AudioFile* file);
+    AudioFile* sfx_load_audio(const char* file);
+    void sfx_free_audio(AudioFile* file);
 
-    AudioSource* create_source();
-    void free_source(AudioSource* source);
+    AudioSource* sfx_create_source(AudioFile* file);
+    void sfx_free_source(AudioSource* source);
 
-    AudioSource* play_audio(AudioFile* file);
-    void play_source(AudioSource* source);
+    AudioSource* sfx_play_audio(AudioFile* file);
+    void sfx_play_source(AudioSource* source);
 
-    AudioSource* play_streaming_audio(AudioFile* file);
+    AudioSource* sfx_play_streaming_audio(AudioFile* file);
 
-    void pause_audio(AudioSource* source);
-    void stop_audio(AudioSource* source);
+    void sfx_pause(AudioSource* source);
+    void sfx_stop(AudioSource* source);
 
-    void set_sample_position(AudioSource* source, unsigned int position);
-    unsigned int get_sample_position(AudioSource* source);
+    void sfx_set_sample_position(AudioSource* source, unsigned int position);
+    unsigned int sfx_get_sample_position(AudioSource* source);
 
-    enum AudioStatus get_status(AudioSource* source);
+    enum AudioStatus sfx_get_status(AudioSource* source);
 
-    void set_audible_distance(AudioSource* source, float min, float max);
-    void set_looping(AudioSource* source, bool looping);
-    void set_position(AudioSource* source, float x, float y, float z);
-    void set_direction(AudioSource* source, float x, float y, float z);
-    void set_velocity(AudioSource* source, float x, float y, float z);
-    void set_volume(AudioSource* source, float volume);
-    void set_pitch(AudioSource* source, float pitch);
-    void set_frequency(AudioSource* source, int frequency);
-    void set_cone(AudioSource* source, float innerAngle, float outerAngle, float outerVolume);
+    void sfx_set_audible_distance(AudioSource* source, float min, float max);
+    void sfx_set_looping(AudioSource* source, bool looping);
+    void sfx_set_position(AudioSource* source, float x, float y, float z);
+    void sfx_set_direction(AudioSource* source, float x, float y, float z);
+    void sfx_set_velocity(AudioSource* source, float x, float y, float z);
+    void sfx_set_volume(AudioSource* source, float volume);
+    void sfx_set_pitch(AudioSource* source, float pitch);
+    void sfx_set_frequency(AudioSource* source, int frequency);
+    void sfx_set_cone(AudioSource* source, float innerAngle, float outerAngle, float outerVolume);
+    void sfx_set_relative(AudioSource* source, bool relative);
 
+    void sfx_set_listener_position(float x, float y, float z);
+    void sfx_set_listener_velocity(float x, float y, float z);
+    void sfx_set_listener_orientation(float forwardX, float forwardY, float forwardZ, float upX, float upY, float upZ);
 
 #ifdef __cplusplus
 }
